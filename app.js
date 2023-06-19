a0b1281b93c33835a5360389e3e29680464b67ba
eugenetangkj <dragoneugene10@gmail.com>
2023-06-20 00:57:30 +0800

    Start on about me section

--- app.js
+++ app.js
@@ -3,26 +3,23 @@ const hamburgerIcon = document.querySelector("#hamburger-icon");
 const closeIcon = document.querySelector("#close-icon");
 const hamburgerMenuItems = document.querySelector("#hamburger-menu-items");
 const hamburgerMenuList = document.querySelector("#hamburger-menu-ul");
+const bodyElement = document.getElementsByTagName("BODY")[0];
 
-
-
+//Runs when hamburger menu is pressed on
 function openMenu() {
     hamburgerIcon.classList.toggle("hidden");
     closeIcon.classList.toggle("hidden");
     hamburgerMenuItems.classList.add('w-[100%]');
     hamburgerMenuItems.classList.remove('w-0');
-    // hamburgerMenuList.classList.remove('mr-96');
-
-
-
+    bodyElement.classList.add('overflow-hidden');
 }
 
+//Runs when cross button is pressed on
 function closeMenu() {
     hamburgerIcon.classList.toggle("hidden");
     closeIcon.classList.toggle("hidden");
     hamburgerMenuItems.classList.add('w-0');
     hamburgerMenuItems.classList.remove('w-[100%]');
-    // hamburgerMenuList.classList.add('mr-96');
-
-
+    bodyElement.classList.remove('overflow-hidden');
 }
+
