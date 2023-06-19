1b10bd4bb68a0469d197168a8b70b2692300105a
eugenetangkj <dragoneugene10@gmail.com>
2023-06-19 22:51:54 +0800

    Attempt at mobile menu animation

--- app.js
+++ app.js
@@ -1,14 +1,28 @@
 //Make hamburger menu interactive
-const hamburgerMenu = document.querySelector("#hamburger-menu");
+const hamburgerIcon = document.querySelector("#hamburger-icon");
+const closeIcon = document.querySelector("#close-icon");
 const hamburgerMenuItems = document.querySelector("#hamburger-menu-items");
-const hamburgerMenuLines = document.getElementsByClassName("hamburger-menu-lines");
-console.log(hamburgerMenuLines);
+const hamburgerMenuList = document.querySelector("#hamburger-menu-ul");
 
-hamburgerMenu.addEventListener("click", () => {
-    hamburgerMenuItems.classList.toggle("hidden");
-    var i;
-    for (i = 0; i < hamburgerMenuLines.length; i = i + 1) {
-        hamburgerMenuLines[i].classList.toggle("bg-white");
-        hamburgerMenuLines[i].classList.toggle("bg-black");
-    }
-})
+
+
+function Open() {
+    hamburgerIcon.classList.toggle("hidden");
+    closeIcon.classList.toggle("hidden");
+    hamburgerMenuItems.classList.add('w-[100%]');
+    hamburgerMenuItems.classList.remove('w-0');
+    hamburgerMenuList.classList.remove('mr-96');
+
+
+
+}
+
+function Close() {
+    hamburgerIcon.classList.toggle("hidden");
+    closeIcon.classList.toggle("hidden");
+    hamburgerMenuItems.classList.add('w-0');
+    hamburgerMenuItems.classList.remove('w-[100%]');
+    hamburgerMenuList.classList.add('mr-96');
+
+
+}
