b6353ba239a8af3d8cf9bf054ce4e8bab8e5cfef
eugenetangkj <dragoneugene10@gmail.com>
2023-11-23 00:32:04 +0800

    Add cel information

--- app.js
+++ app.js
@@ -46,16 +46,16 @@ moonIconMenu.addEventListener("click", () =>
 
 
 /* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
-var prevScrollpos = window.pageYOffset;
-window.onscroll = function() {
-  var currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
-    document.getElementById("navbar").style.top = "0";
-  } else {
-    document.getElementById("navbar").style.top = "-200px";
-  }
-  prevScrollpos = currentScrollPos;
-}
+// var prevScrollpos = window.pageYOffset;
+// window.onscroll = function() {
+//   var currentScrollPos = window.pageYOffset;
+//   if (prevScrollpos > currentScrollPos) {
+//     document.getElementById("navbar").style.top = "0";
+//   } else {
+//     document.getElementById("navbar").style.top = "-200px";
+//   }
+//   prevScrollpos = currentScrollPos;
+// }
 
 
 
